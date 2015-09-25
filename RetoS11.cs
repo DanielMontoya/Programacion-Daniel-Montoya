67619a51aeebe27b9e6d65457538ebf255d073f9
DanielMontoya <danymf96@yahoo.com.co>
2015-09-25 16:25:05 -0500

    Reto Semana 11

---
+++ RetoS11.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(CountTo100);
+            thread1.Start("thread1");
+            Thread thread2 = new Thread(CountTo100);
+            thread2.Start("thread2");
+            thread1.Join();
+            thread2.Join();
+            Console.ReadKey();
+        }
+
+        public static void CountTo100(object thread)
+        {
+            string name = (string)thread;
+            
+            for (int index = 0; index < 100; index++)
+                Console.WriteLine(name + ":" + (index + 1));
+                
+        }
+    }
+}
